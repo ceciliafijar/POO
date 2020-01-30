bc39c44def00b607fbc10713fe08374e4116444a
ceciliafijar <fijarcecilia@gmail.com>
2020-01-30 13:14:10 -0700

    Subi mi primer proyecto

---
+++ MiPrimerProyectoConsola/MiPrimerProyectoConsola/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerProyectoConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.Write("Introduce un numero: ");
+            a = Convert.ToInt32 (Console.ReadLine());
+            
+            Console.Write("Introdusca otro numero: ");
+            b = Convert.ToInt32 (Console.ReadLine());
+
+            c = a + b;
+
+            Console.WriteLine("La suma de los numeros es: " + c);
+
+            Console.ReadKey();
+                
+
+
+
+
+
+
+        }
+    }
+}
