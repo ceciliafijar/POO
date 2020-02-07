2d7f07f62df68288257a51b0f97ac7da8932ebb9
ceciliafijar <60377290+ceciliafijar@users.noreply.github.com>
2020-02-06 22:49:42 -0700

    Add files via upload

---
+++ Embotalladora/Embotalladora/ClassBotella.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Embotalladora
+{
+    class ClassBotella
+    {
+        //Atributos de la clase
+        public int nivelLlenado;
+        int mililitrosVerificados;
+
+        //Metodos de la clase
+        public String verificarLlenado()
+        {
+            String mensaje;
+            if (nivelLlenado == 250)
+                mensaje = "Llenado Perfecto";
+            else if (nivelLlenado < 250)
+            {
+                mililitrosVerificados = 250 - nivelLlenado;
+                mensaje = "Le faltan "+ mililitrosVerificados + " mililitros";
+            }
+            else
+            {
+                mililitrosVerificados = nivelLlenado - 250;
+                mensaje = "Le sobran" + mililitrosVerificados + " militros";
+            }
+
+            return mensaje;
+
+        }
+    }
+}
