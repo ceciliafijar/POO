1ec5dca846df3987dabe311cbdcccec3b8ccb3b8
ceciliafijar <60377290+ceciliafijar@users.noreply.github.com>
2020-02-13 21:07:44 -0700

    Add files via upload

---
+++ CalculadoraDePromedios/CalculadoraDePromedios/Calificacion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDePromedios
+{
+    public class Calificacion
+    {
+        //Atributos de la clase 
+        public decimal calLeida = 0, promedioGrupal, sumaCalif;
+        public int aprobados = 0;
+        public int reprobados = 0;
+
+        //Metodos de la clase
+        public void contarAprobacion()
+
+        {
+            if (calLeida < 70)
+                reprobados++;
+            else
+                aprobados++;
+
+        }
+        public void sumarCalificacion()
+        {
+            sumaCalif += calLeida;
+        }
+        public decimal calcularPromedio()
+        {
+            promedioGrupal = sumaCalif / (aprobados + reprobados);
+            return promedioGrupal;
+        }
+            
+      
+    }
+}
